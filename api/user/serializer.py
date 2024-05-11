6776916c24e055b3a9c4c708540e6198fbf97472
MZaFaRM <muhammedzafar.mm@gmail.com>
2024-05-12 00:37:08 +0530

    [PATCH] Login patch

--- api/user/serializer.py
+++ api/user/serializer.py
@@ -8,3 +8,6 @@ class UserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ["mobile", "password", "username", "first_name"]
         extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        return CustomUser.objects.create_user(**validated_data)
